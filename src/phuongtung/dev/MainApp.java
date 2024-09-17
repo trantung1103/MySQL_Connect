1dc70bed5686f127d2f4fb0581d266a643d1467e
Admin <Admin@LAPTOP-JLFISU36>
2024-09-17 08:48:17 +0700

    first

---
+++ src/phuongtung/dev/MainApp.java
@@ -0,0 +1,23 @@
+package phuongtung.dev;
+
+import java.sql.DriverManager;
+
+public class MainApp {
+	
+	static final String DB_URL = "jdbc:mysql://localhost/shop";
+	static final String DB_USER  = "root";
+	static final String DB_PASS  = "";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+			System.out.println("Ket noi thanh cong");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
